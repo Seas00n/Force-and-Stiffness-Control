fix bug of DH, add jacobian matrix of revolute
positons of each joint is just the last col of H matrix

armStructure.py
@@ -1,12 +1,100 @@
-import rigidBody.rigidBody
+from rigidBody import rigidBody
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from pdb import set_trace
+
+debug_info = 0
 
 class armStructure(object):
 
     def __init__(self):
-        self.forward_kniematics_matrix = None;
+        self.TMatrixs = []#[T_{01}, T_{02}, T_{03}, ....]
+        self.TMatrix = np.array([ [1,0,0,0],
+                                  [0,1,0,0],
+                                  [0,0,1,0],
+                                  [0,0,0,1]])
+        self.links = []
+        self.num_links = 0
+        self.joints = []
+        self.jacob_matrix = np.zeros([6, 0])
 
-    def forwardKinematics(self ):
-        pass
+    def addLink(self, joint_type, theta, d, a, alpha, link_name):
+        link = rigidBody(joint_type,theta,d,a,alpha,link_name)
+        self.links.append(link)
+        self.num_links += 1
 
+    def forwardKinematics(self):
+        for i in range(self.num_links):
+            self.TMatrix = self.TMatrix.dot(self.links[i].AMatrix)
+            if debug_info:
+                print('T:      \n',self.TMatrix)
+                print('A:     \n', self.links[i].AMatrix)
+            self.TMatrixs.append(self.TMatrix)
+            #print(self.TMatrix)
+            if i==0:
+                self.links[i].calculateJointCoord(np.array((0,0,0)), TMatrix=self.TMatrix)
+            else:
+                self.links[i].calculateJointCoord(last_joint_coord=self.links[i-1].end_joint_coord, TMatrix=self.TMatrix)
+     
+    def jacobianCalc(self):
+        self.jacob_matrix = np.zeros([6, self.num_links-1])
+        for i in range(self.num_links):
+            if self.links[i].joint_type == 'nojoint':
+                continue
+            elif self.links[i].joint_type == 'prismatic':
+                print('prismatic not yet')
+                exit()
+            elif self.links[i].joint_type == 'revolute':
+                '''
+                [j_vi] = Z_0_i-1 X (d_0_n - d_0_i-1)
+                [j_wi] = Z_0_i-1
+                joint i -> frame i-1
+                '''      
+                z_0_iminus1 = self.TMatrixs[i-1][:-1, 2]
+                d_0_iminus1 = self.TMatrixs[i-1][:-1,-1]
+                d_0_n       = self.TMatrix[:-1,-1]
+                self.jacob_matrix[:3,i-1] = np.cross(z_0_iminus1, (d_0_n-d_0_iminus1))
+                self.jacob_matrix[3:,i-1] = z_0_iminus1   
+            else:
+                print('error') 
+                exit()  
 
+    def visualize(self):
+        x=[]
+        y=[]
+        z=[]
+        for i in range(self.num_links):
+            a,b,c = self.links[i].visualize()
+            x.append(a)
+            y.append(b)
+            z.append(c)
+        return x,y,z
+
+if __name__ == '__main__':
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    robotic_arm = armStructure()
+    robotic_arm.addLink('nojoint',    0,  0,    0,  0,'link0')
+    robotic_arm.addLink('revolute',  10,  0, 0.14,  0,'link1')
+    robotic_arm.addLink('revolute', 110,  0, 0.12,  0,'link2')
+    #robotic_arm.addLink('revolute',   0,  10, 0,  0,'link1')
+    #robotic_arm.addLink('revolute',   0,  10, 0,  -90,'link2')
+    #robotic_arm.addLink('revolute',   0,  10, 0,  0,'link3')
+
+    robotic_arm.forwardKinematics()
+    robotic_arm.jacobianCalc()
+    x,y,z = robotic_arm.visualize()
+    #print(robotic_arm.jacob_matrix)
+    for c in range(len(x)):
+        i = x[c]
+        j = y[c]
+        k = z[c]
+        ax.plot(i,j,k,'o-')
+    plt.show()
 

rigidBody.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-JOINT_TYPE = ['revolute', 'prismatic']
+from pdb import set_trace
+JOINT_TYPE = ['revolute', 'prismatic', 'nojoint']
+show_axis = 1
 
 class rigidBody(object):
     '''
@@ -11,29 +12,33 @@ class rigidBody(object):
     link twist  alpha
     joint angle theta
     '''
-    fig = plt.figure()
-    ax = Axes3D(fig)
+    #fig = plt.figure()
+    #ax = Axes3D(fig)
 
-    def __init__(self, joint_type, theta, d, a, alpha, link_name, if_base=0):
-        self.theta = theta
+    def __init__(self, joint_type, theta, d, a, alpha, link_name, fix_frame_coord=np.zeros([3,1])):
+        self.theta = theta/180.0*np.pi
         self.d     = d
         self.a     = a
-        self.alpha = alpha
+        self.alpha = alpha/180*np.pi
         self.AMatrix = self.generateHomogeneousMatrix()
-        self.head_joint_coord = np.zeros([3,1]) #x0,y0,z0
-        self.end_joint_coord = np.zeros([3,1]) #x0,y0,z0
+        self.head_joint_coord = fix_frame_coord#x0,y0,z0
+        self.end_joint_coord = np.zeros([3,1]) #x1,y1,z1
         self.joint_type = joint_type
-        self.if_base = if_base
         self.link_name = link_name
-        self.generateHomogeneousMatrix()
+        #rigidBody.ax.set_xlabel('x')
+        #rigidBody.ax.set_ylabel('y')
+        #rigidBody.ax.set_zlabel('z')
 
     def setParameter(self, arg):
         if self.joint_type ==JOINT_TYPE[0]:
             #revolute
             self.theta = arg
-        else:
+        elif self.joint_type == JOINT_TYPE[1]:
             #prismatic
             self.d = arg
+        else:
+            #base link0
+            pass
 
     def moveIt(self):
         pass
@@ -42,11 +47,11 @@ class rigidBody(object):
         '''
         rot_theta, trans_d: along axis Z{i-1}
         trans_a, rot_alpha: along axis X{i}
-        '''
+        ''' 
         rot_theta = np.array([[np.cos(self.theta), -np.sin(self.theta), 0, 0],
-                              [np.sin(self.theta), np.cos(self.theta),  0, 0],
-                              [0,0,1,0],
-                              [0,0,0,1]])
+                              [np.sin(self.theta),  np.cos(self.theta), 0, 0],
+                              [0                 ,                   0, 1, 0],
+                              [0                 ,                   0, 0, 1]])
 
         trans_d = np.array([[1, 0, 0, 0],
                             [0, 1, 0, 0],
@@ -63,27 +68,38 @@ class rigidBody(object):
                               [0, np.sin(self.alpha), np.cos(self.alpha),  0],
                               [0, 0,                  0,                   1]])
 
-        AMatrix = rot_theta.dot(trans_d).dot(trans_a).dot(rot_alpha)
+        #AMatrix = ((rot_theta.dot(trans_d)).dot(trans_a)).dot(rot_alpha)
+        AMatrix = np.dot(rot_theta, np.dot(trans_d, np.dot(trans_a, rot_alpha)))
+        AMatrix[np.where((AMatrix<0.00001) & (AMatrix>0))]=0
+        AMatrix[np.where((AMatrix>-0.00001) & (AMatrix<0))]=0
+
         return  AMatrix
 
-    def calculateJointCoord(self, last_joint_coord):
+    def calculateJointCoord(self, last_joint_coord, TMatrix):
         #self.joint_coord = np.dot(self.AMatrix, last_joint_coord)
-        #self.head_joint_coord = self.AMatrix[:3,-1] + last_joint_coord
-        #self.end_joint_coord  = self.AMatrix[:3,:3].dot(self.head_joint_coord)*self.a
-        self.head_joint_coord = last_joint_coord
-        self.end_joint_coord  = self.AMatrix[:3,-1] + last_joint_coord
+        self.head_joint_coord = last_joint_coord.reshape((3,1))
+        self.end_joint_coord  = TMatrix[:3,-1].reshape((3,1))
+        #set_trace()
 
-    def visualize(self, last_joint_coord):
-        self.calculateJointCoord(last_joint_coord)
-        rigidBody.ax.set_xlabel('x')
-        rigidBody.ax.set_ylabel('y')
-        rigidBody.ax.set_zlabel('z')
+    def visualize(self):
+        #self.calculateJointCoord(last_joint_coord)
+        #print(self.head_joint_coord[0], self.end_joint_coord[0])
         x = np.linspace(self.head_joint_coord[0], self.end_joint_coord[0], 100)
         y = np.linspace(self.head_joint_coord[1], self.end_joint_coord[1], 100)
         z = np.linspace(self.head_joint_coord[2], self.end_joint_coord[2], 100)
-        rigidBody.ax.plot(x,y,z,'o-')
-        #plt.show()
-
+        return x,y,z
+        #rigidBody.ax.plot(x,y,z,'o-')
+    '''
+    def show_axis(self):
+        if show_axis:
+            x_axis = self.AMatrix[:,0]
+            y_axis = self.AMatrix[:,1]
+            z_axis = self.AMatrix[:,2]
+            for axis in [x_axis, y_axis, z_axis]:
+                x = np.linspace(-50, 50, 100)
+                y = np.linspace(self.head_joint_coord[1], self.end_joint_coord[1], 100)
+                z = np.linspace(self.head_joint_coord[2], self.end_joint_coord[2], 100)
+    '''
     def getInfo(self):
         print(self.link_name,'{')
         print('  theta:', self.theta, '\n  d:', self.d, '\n  a:', self.a, '\n  alpha:', self.alpha, '\n  AMatrix:', self.AMatrix)
@@ -92,11 +108,15 @@ class rigidBody(object):
         print('}')
 
 if __name__ == '__main__':
-    link0 = rigidBody('revolute', theta=0, d=0, a=0, alpha=0, if_base=1, link_name='link0')
+
+    link0 = rigidBody('nojoint', theta=0, d=50, a=0, alpha=0, link_name='link0',head_joint_coord=np.array((1,2,3)))
     link0.visualize(np.array([0,0,0]))
     link0.getInfo()
 
-    link1 = rigidBody('revolute', theta=0, d=0, a=100, alpha=0, link_name='link1')
-    link1.visualize(link0.end_joint_coord)
-    link1.getInfo()
+    #link1 = rigidBody('revolute', theta=0, d=20, a=0, alpha=0, link_name='link1')
+    #link1.visualize(np.array([0,0,0]))
+    #link1.getInfo()
     plt.show()
+
+
+